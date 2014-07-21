6afee5ae2d94fe54538cbd639989c70b382b5591
Harry Schmidt <goldibex@casetext.com>
2014-07-21 11:26:36 -0700

    Up timeout intervals for tests (sometimes the API is slow)

--- script/test.bash
+++ script/test.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 source ./test/prep.bash
-./node_modules/mocha/bin/mocha --recursive -u bdd -t 10000 -s 1000 -r ./test/setup test/spec
+./node_modules/mocha/bin/mocha --recursive -u bdd -t 30000 -s 2000 -r ./test/setup test/spec
